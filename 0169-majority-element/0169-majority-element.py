96bd44243e3932bf8a29d7658a70380f89a1e0b1
Apurva Patel <145287094+Apurva3509@users.noreply.github.com>
2025-02-13 16:12:48 -0500

    Time: 9 ms (60.06%), Space: 13.5 MB (96.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,12 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res = 0
-        maxCount = 0
-        count = {}
-
+        # Boyer - Moore Algorithm
+        count, res = 0, 0
+        
         for n in nums:
-            count[n] = 1 + count.get(n, 0)
-            res = n if count[n]>maxCount else res
-            maxCount = max(count[n],maxCount)
+            if count == 0:
+                res = n
+
+            count += (1 if res == n else -1)
         return res
+
+
+
+
+        # T: O(n) S: O(n)
+        # res = 0
+        # maxCount = 0
+        # count = {}
+
+        # for n in nums:
+        #     count[n] = 1 + count.get(n, 0)
+        #     res = n if count[n]>maxCount else res
+        #     maxCount = max(count[n],maxCount)
+        # return res
